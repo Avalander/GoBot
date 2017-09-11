1df3e65e27e95c77c210e0992d08b2628ba6b29f
Avalander <darth.quesorico@gmail.com>
2017-09-11 15:47:34 +0200

    Rename uptime constants to follow golang conventions

--- src/modules/uptime/uptime.go
+++ src/modules/uptime/uptime.go
@@ -10,11 +10,11 @@ var (
 	start time.Time
 )
 
-const SECOND = 1
-const MINUTE = 60
-const HOUR = MINUTE * 60
-const DAY = HOUR * 24
-const WEEK = DAY * 7
+const second = 1
+const minute = 60
+const hour = minute * 60
+const day = hour * 24
+const week = day * 7
 
 func init() {
 	start = time.Now()
@@ -24,11 +24,11 @@ func SendUptime(SendMessage func(string)) {
 	i, _ := strconv.ParseInt("1504010580", 10, 64)
 	uptime := int64(time.Now().Sub(time.Unix(i, 0)).Seconds())
 	text := fmt.Sprintf("I've been up for %d weeks, %d days, %d hours, %d minutes and %d seconds.",
-		convertDurationTo(WEEK, &uptime),
-		convertDurationTo(DAY, &uptime),
-		convertDurationTo(HOUR, &uptime),
-		convertDurationTo(MINUTE, &uptime),
-		convertDurationTo(SECOND, &uptime))
+		convertDurationTo(week, &uptime),
+		convertDurationTo(day, &uptime),
+		convertDurationTo(hour, &uptime),
+		convertDurationTo(minute, &uptime),
+		convertDurationTo(second, &uptime))
 	SendMessage(text)
 }
 
